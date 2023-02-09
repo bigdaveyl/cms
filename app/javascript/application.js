2bfb131c58f0dcd457b0d1f797ff8fd40fd61b4f
David Liana <dliana@frontiernet.net>
2023-02-09 07:17:27 -0500

    handle flash messages in application.html.erb

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,1 +1,6 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+import "jquery"
+import "jquery_ujs"
+import "jquery-ui"
+import "popper"
+import "bootstrap"
--- config/importmap.rb
+++ config/importmap.rb
@@ -1,3 +1,9 @@
 # Pin npm packages by running ./bin/importmap
 
+pin "jquery3", to: "jquery3.min.js", preload: true
+pin "jquery_ujs", to: "jquery_ujs.js", preload: true
+pin "jquery-ui", to: "jquery-ui.js", preload: true
+pin "popper", to: "popper.js", preload: true
+pin "bootstrap", to: "bootstrap.min.js", preload: true
+
 pin "application", preload: true
