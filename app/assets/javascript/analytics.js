fd1194509acf8a39737836abb941839250c2cbb4
David Liana <dliana@frontiernet.net>
2023-02-09 07:26:25 -0500

    updates to analytics.js, with data sent to google

--- app/assets/javascript/analytics.js
+++ app/assets/javascript/analytics.js
@@ -4,9 +4,15 @@ function gtag() { dataLayer.push(arguments); }
 gtag('js', new Date());
 
 gtag('config', 'G-1E3GVJJG8D', { 
-    'page_location': window.location,
-    'page_path':   window.location.pathname,
-    'page_origin': window.location.origin,
+    'page_location': document.location,
+    'page_path':   document.location.pathname,
+    'page_origin': document.location.origin,
     'page_title': document.title,
+    'page_host': document.location.host,
+    'page_port': document.location.port,
+    'page_protocol': document.location.protocol,
+    'page_hash': document.location.hash,
+    'page_href': document.location.href,
+    'page_search': document.location.search,
     'cookie_flags': 'max-age=7200;secure;samesite=none'
 });
