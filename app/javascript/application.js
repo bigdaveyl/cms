e28208d13009ba5204d5c51b5e00f6bde9a4b980
David Liana <dliana@frontiernet.net>
2023-02-09 09:20:13 -0500

    update jquery to jquery3 in application.js

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,5 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "jquery"
+import "jquery3"
 import "jquery_ujs"
 import "jquery-ui"
 import "popper"
