414f11949d9dad774b55bdeb81ec8a8d032234eb
David Liana <dliana@frontiernet.net>
2023-02-09 07:22:44 -0500

    make analytics.js for google analytics

---
+++ app/assets/javascript/analytics.js
@@ -0,0 +1,12 @@
+window.dataLayer = window.dataLayer || [];
+function gtag() { dataLayer.push(arguments); }
+
+gtag('js', new Date());
+
+gtag('config', 'G-1E3GVJJG8D', { 
+    'page_location': window.location,
+    'page_path':   window.location.pathname,
+    'page_origin': window.location.origin,
+    'page_title': document.title,
+    'cookie_flags': 'max-age=7200;secure;samesite=none'
+});
